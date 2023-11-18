10ff2c546cf9d1631b487126081672bda5a0b948
jesusra11 <130915166+jesusra11@users.noreply.github.com>
2023-11-18 12:19:47 -0400

    Update hack_10.py

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,11 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    caract=[('o','0'),('i','1'),('a','@')]
+
+    for i in range(len(caract)):
+        str_sear, str_rep= caract[i]
+        if result.find(str_sear):            
+            result= result.replace(str_sear, str_rep) 
+
+    return list(result.upper()) 
